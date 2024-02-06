4b48d5b1227b48f8d547e587d0f95f9b09fb4315
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-02-06 23:46:30 +0900

    unions

--- main.go
+++ main.go
@@ -34,7 +34,11 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n", SumIntsOrFloats(ints), SumIntsOrFloats(floats))
 }
 
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+type Number interface {
+	int64 | float64
+}
+
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
